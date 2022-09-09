8dee0c1b1c2740e8e9e1884c0d8605665eb0bed5
Trapland <89914789+Trapland@users.noreply.github.com>
2022-09-09 17:12:34 +0200

    Добавьте файлы проекта.

---
+++ ResourceImage/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// ресурс добавлен с вынесенным исполняемым файлом все работает отлично
+
+namespace ResourceImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
